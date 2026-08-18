a7249756b959b90342e6f51ca29244047c1eb80b
kezhang29 <kevinzhang.oak@gmail.com>
2026-08-17 20:51:20 -0700

    8-17 class

---
+++ week-2/260817/Error.java
@@ -0,0 +1,8 @@
+public class Error {
+	public static void main(String[] args) {
+        // This is a runtime error because of dividing by 0
+		// System.out.println("0/9 is " + (9/0));
+        // Fixed code:
+        System.out.println("0/9 is " + (0/9));
+	}
+}
